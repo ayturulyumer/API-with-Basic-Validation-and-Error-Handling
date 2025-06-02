552a1ca2db96bca55874534475af1c90e87d9307
ajummer <makeudreamempty@gmail.com>
2025-06-02 14:26:05 +0300

    feat: create add item function

--- server/src/services/itemService.ts
+++ server/src/services/itemService.ts
@@ -1,6 +1,6 @@
 // src/services/itemsService.ts
 import { v4 as uuidv4 } from "uuid";
-import { Item } from "../types/Item";
+import { Item } from "../types/item";
 
 let items: Item[] = [];
 
@@ -8,3 +8,8 @@ export function getAllItems(): Item[] {
   return items;
 }
 
+export function addItem(name: string): Item {
+  const newItem: Item = { id: uuidv4(), name };
+  items.push(newItem);
+  return newItem;
+}
