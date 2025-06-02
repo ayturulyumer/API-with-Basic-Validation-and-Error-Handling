5c81d31c9aa3f8869fc75c464e955ee17895e6c4
ajummer <makeudreamempty@gmail.com>
2025-06-02 21:16:01 +0300

    Initial commit

--- server/src/controllers/itemController.ts
+++ server/src/controllers/itemController.ts
@@ -1,7 +1,7 @@
 // controllers/itemController.ts
 import { Router, Request, Response } from "express";
-import { Item } from "../types/item";
-import { addItem, getAllItems } from "../services/itemService"; 
+import { Item } from "../types/Item";
+import { addItem, getAllItems, getItemById } from "../services/itemService";
 
 const router = Router();
 
@@ -19,13 +19,33 @@ router.post("/", (req: Request, res: Response) => {
   try {
     const { name } = req.body;
     if (!name) {
-      return res.status(400).json({ message: "Item name is required" });
+      res.status(400).json({ message: "Item name is required" });
+      return;
     }
 
     const newItem: Item = addItem(name);
     res.status(201).json(newItem);
   } catch (error) {
     console.error("Error adding item:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/:id", (req: Request, res: Response) => {
+  const itemId = req.params.id;
+  console.log(itemId);
+  try {
+    const item = getItemById(itemId);
+    console.log("Fetching item with ID:", itemId);
+
+    if (!item) {
+      res.status(404).json({ message: "Item not found" });
+      return;
+    }
+
+    res.status(200).json(item);
+  } catch (error) {
+    console.error("Error fetching item:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
--- server/src/services/itemService.ts
+++ server/src/services/itemService.ts
@@ -1,6 +1,6 @@
-// src/services/itemsService.ts
+
 import { v4 as uuidv4 } from "uuid";
-import { Item } from "../types/item";
+import { Item } from "../types/Item";
 
 let items: Item[] = [];
 
@@ -12,4 +12,8 @@ export function addItem(name: string): Item {
   const newItem: Item = { id: uuidv4(), name };
   items.push(newItem);
   return newItem;
+}
+
+export function getItemById(id: string): Item | undefined {
+  return items.find(item => item.id === id);
 }
